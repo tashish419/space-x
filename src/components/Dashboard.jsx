e737fc314d8b393dc8a6021db72afecc8651dc24
Ashish Tomar <tashish410@gmail.com>
2024-08-03 18:22:30 +0530

    created dashboard component and added css

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,19 @@
+import { BG_URL } from "../assets/constants";
 import LaunchCard from "./LaunchCard";
 
 
 const Dashboard = () => {
   return (
-    <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+    <div className="flex-1 p-6 bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white relative rounded-r-lg">
+      <img src={BG_URL} alt="Bg-img" className="absolute inset-0 w-[98%] h-full object-cover"/>
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="relative z-10 grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-6">
 
         {/* upcoming launch */}
-        <div className="p-6 bg-gray-800 rounded-lg shadow-lg">
+        <div className="p-6 bg-gray-800 rounded-lg shadow-lg bg-opacity-90 text-white">
             <LaunchCard/>
         </div>
+        
 
         {/* previous launch */}
         <div className="p-6 bg-gray-800 rounded-lg shadow-lg">
@@ -26,7 +30,7 @@ const Dashboard = () => {
             <LaunchCard/>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
