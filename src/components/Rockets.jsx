b69f8aa80a5dc7bcfa41ac40389760d598fc1c4d
Ashish Tomar <tashish410@gmail.com>
2024-08-05 14:04:40 +0530

    styled rockets tab UI && made page dynamic

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -10,25 +10,64 @@ const Rockets = () => {
   console.log(rocketsData);
   useRocketsData();
   return (
-    <div className="flex-1 justify-center items-center bg-gradient-to-r shadow-lg from-gray-700 via-gray-900 to-black relative rounded-r-lg">
+    <div className="flex items-center justify-center  bg-gradient-to-r from-gray-700 via-gray-900 to-black relative rounded-r-lg scrollbar">
       <img
         src={bgImg}
         alt="Bg-img"
         className="absolute inset-0 w-full h-full object-cover rounded-r-lg"
       />
-      <div className="rounded-[15px] bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-[rgba(0,0,0,0.6)] relative z-20 p-4 w-[820px] mx-auto">
-        <div className="flex justify-evenly">
+      <div className="rounded-[15px] bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-[rgba(0,0,0,0.6)] relative z-20 p-6  w-[820px] mx-14 ">
+        <div className="flex justify-around gap-4 ">
           <div>
-            <h2>{rocketsData[0]?.rocket_name}</h2>
-            <img src={rocketImg1} alt="rocket-img-1" loading="lazy" />
+            <h2 className="text-white font-semibold text-lg mb-2">
+              {rocketsData[0]?.rocket_name}
+            </h2>
+            <div className="relative h-72">
+              <img
+                src={rocketImg1}
+                alt="rocket-img-1"
+                loading="lazy"
+                className="rounded-lg h-full object-cover shadow-lg"
+              />
+              <div className="p-1 absolute z-10 bottom-2 left-2 rounded-lg bg-yellow-600 bg-opacity-70 flex flex-col justify-start items-start my-1">
+                <h4 className="text-xs font-semibold text-gray-400">STATUS</h4>
+                <p className="text-sm font-bold text-white">{rocketsData[0]?.active ? "Active" : "In development"}</p>
+              </div>
+            </div>
           </div>
           <div>
-            <h2>{rocketsData[1]?.rocket_name}</h2>
-            <img src={rocketImg2} alt="rocket-img-2" loading="lazy" />
+            <h2 className="text-white font-semibold text-lg mb-2">
+              {rocketsData[1]?.rocket_name}
+            </h2>
+            <div className="relative h-72">
+              <img
+                src={rocketImg2}
+                alt="rocket-img-2"
+                loading="lazy"
+                className="rounded-lg h-full object-cover shadow-lg"
+              />
+              <div className="p-1 absolute z-10 bottom-2 left-2 rounded-lg bg-green-600 bg-opacity-70 flex flex-col justify-start items-start my-1">
+                <h4 className="text-xs font-semibold text-gray-400">STATUS</h4>
+                <p className="text-sm font-bold text-white">{rocketsData[1]?.active ? "Active" : "In development"}</p>
+              </div>
+            </div>
           </div>
           <div>
-            <h2>{rocketsData[2]?.rocket_name}</h2>
-            <img src={rocketImg3} alt="rocket-img-3" loading="lazy" />
+            <h2 className="text-white font-semibold text-lg mb-2">
+              {rocketsData[2]?.rocket_name}
+            </h2>
+            <div className="relative h-72">
+              <img
+                src={rocketImg3}
+                alt="rocket-img-3"
+                loading="lazy"
+                className="rounded-lg h-full object-cover shadow-lg"
+              />
+              <div className="p-1 absolute z-10 bottom-2 left-2 rounded-lg bg-green-600 bg-opacity-70 flex flex-col justify-start items-start my-1">
+                <h4 className="text-xs font-semibold text-gray-400">STATUS</h4>
+                <p className="text-sm font-bold text-white">{rocketsData[2]?.active ? "Active" : "In development"}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
